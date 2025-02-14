273dde20be0cb589c3e7302b59ae0e202764ad7f
ZhangLabTJU <89921026+ZhangLabTJU@users.noreply.github.com>
2025-02-14 09:47:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,43 @@ from setuptools.command.install import install
 from BCR_V_BERT.utils.download_model import clone_huggingface_repo
 
 here = path.abspath(path.dirname(__file__))
+
 class CustomInstallCommand(install):
     """Customized install command to download models."""
-    def run(self):
-        install.run(self)
+    def run(self) -> None:
+        super().run()
         repo_url = "https://huggingface.co/xqh/BCR-V-BERT"
         destination_dir = path.join(here, "BCR_V_BERT/model_pretrained")
         
-        print("Starting to download the model repository...")
-        clone_huggingface_repo(repo_url, destination_dir)
-        print("Model repository downloaded successfully.")
+        print("Starting to download the model repository from Hugging Face...")
+        try:
+            clone_huggingface_repo(repo_url, destination_dir)
+            print("Model repository downloaded successfully.")
+        except Exception as e:
+            print(f"Failed to download the model repository: {e}")
 
-with open(path.join(here, 'requirements.txt'), 'r', encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+def read_file(file_path: str) -> str:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
 
-with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
-    long_description = f.read()
+requirements_file = path.join(here, 'requirements.txt')
+long_description_file = path.join(here, 'README.md')
+
+try:
+    all_reqs = read_file(requirements_file).split('\n')
+    long_description = read_file(long_description_file)
+except FileNotFoundError as e:
+    print(f"Error reading file: {e}")
+    all_reqs = []
+    long_description = ""
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 
 setup(
     name='BCR_V_BERT',  
     version='1.0.0',  
-    author='ZJ-lab',  
-    description='BCR_V_BERT, an antibody-specific transformer language model pre-trained separately on 372M antibody heavy sequences and 3M antibody light sequences. The model can embed the CDR3 or CDR sequences of light and heavy chain antibodies', 
+    author='Qihang Xu, Jian Zhang',  
+    description='BCR_V_BERT, an antibody-specific transformer language model pre-trained separately on 372M antibody heavy sequences and 3M antibody light sequences. The model can embed the CDR3 or CDR sequences of light and heavy chain antibodies for various downstream tasks.', 
     long_description=long_description, 
     long_description_content_type='text/markdown', 
     author_email='jian_zhang@tju.edu.cn',  
@@ -40,7 +53,7 @@ setup(
     },
     install_requires=install_requires, 
     classifiers=[
-        'License :: OSI Approved :: CC BY-NC-SA 4.0',
+        'License :: OSI Approved :: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
     ],
